31ed16f1bb26cfdd20cb581df3d9c2df0f6d959a
LukeHSM <lucashsmartins94@gmail.com>
2025-11-15 20:14:14 -0300

    adjustment

--- Generics/src/Main.kt
+++ Generics/src/Main.kt
@@ -1,25 +1,28 @@
-fun main(){
-    val time = Team("Generic team", mutableListOf(
-        BasketballPlayer("Jorge"),
-        BasketballPlayer("Juca"),
-        FootballPlayer("Felix"),
-        FootballPlayer("Abner")
-        ))
 
-    println(time)
+fun main(){
+    val footballTeam = Team<FootballPlayer>("Football Team", mutableListOf())
+    footballTeam.addMember(FootballPlayer("Jorge"))
+    footballTeam.addMember(FootballPlayer("Kelson"))
+    footballTeam.addMember(FootballPlayer("Botelho Pinto"))
+    println(footballTeam)
+
+    val basketballTeam = Team<BasketballPlayer>("Basketball Team", mutableListOf())
+    basketballTeam.addMember(BasketballPlayer("BiroLiro"))
+    basketballTeam.addMember(BasketballPlayer("Neco Boneco"))
+    basketballTeam.addMember(BasketballPlayer("Omar Melada"))
+    println(basketballTeam)
+
 }
 
 open class Player(val name: String)
 
 class BasketballPlayer(name: String): Player(name){
-    override fun toString(): String {
-        return "Basketball player: ${this.name}"
-    }
+    override fun toString() = "${this.name}"
 }
 class FootballPlayer(name: String): Player(name){
-    override fun toString(): String {
-        return "Football player: ${this.name}"
-    }
+    override fun toString() = "${this.name}"
 }
 
-data class Team<T>(val name: String, val members: List<T>)
+data class Team<T>(val name: String, val members: MutableList<T>){
+    fun addMember(member: T) = members.add(member)
+}
